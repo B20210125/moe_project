6601a9c253444ade99906ee4282d6caf6662298a
Azri Azih <107837364+B20210125@users.noreply.github.com>
2024-01-08 13:14:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "http://165.22.52.48/"; 
-$username = "root"; 
-$password = ""; 
+$username = "admin"; 
+$password = "421cc5e439fb33cc5b0e2bc0226f61d13c24ef707fb05339"; 
 $database = "moe_project"; 
 
 // Create connection
